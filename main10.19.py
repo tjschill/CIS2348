41b121db27f2b187f46fc1ed4a734c54874eabaa
tjschill <66383528+tjschill@users.noreply.github.com>
2020-06-22 10:57:06 -0500

    Update main10.19.py

--- main10.19.py
+++ main10.19.py
@@ -5,17 +5,13 @@ File: 10.19 ZyLab - CIS2348
 """
 
 
-# Type code for classes here
-
-# Type code for classes here
-
 class ItemToPurchase:
 
     def __init__(self, name="none", price=0, quantity=0, item_description="none"):
         self.name = name
         self.price = price
         self.quantity = quantity
-        self.item_description = "none"
+        self.item_description = item_description
 
     def print_item_cost(self):
         total = self.price * self.quantity
@@ -23,7 +19,10 @@ class ItemToPurchase:
         return total
 
     def print_item_description(self):
-        print("{}: {}.".format(self.name, self.item_description))
+        print("{}: {}".format(self.name, self.item_description))
+
+    def total_cost(self):
+        return self.price * self.quantity
 
 
 class ShoppingCart:
@@ -47,19 +46,23 @@ class ShoppingCart:
         print("\nREMOVE ITEM FROM CART")
         remove_name = input("Enter name of item to remove:\n")
         for item in self.cart_items:
-            if item.name == "remove_name":
+            if item.name == remove_name:
                 found = True
                 self.cart_items.remove(item)
         if found == False:
             print("Item not found in cart. Nothing removed.")
 
     def modify_item(self):  # FINISH THIS ONE FIRST
+        found2 = False
         print("\nCHANGE ITEM QUANTITY")
-        modify_name = input("Enter name of item name:\n")
-        modify_quantity = input("Enter the new quantity:\n")
+        modify_name = input("Enter the item name:\n")
+        modify_quantity = int(input("Enter the new quantity:\n"))
         for item in self.cart_items:
-            if item.name == "modify_name":
-                item.quantity = modify_quanitity
+            if item.name == modify_name:
+                found2 = True
+                item.quantity = modify_quantity
+        if found2 == False:
+            print("Item not found in cart. Nothing modified.")
 
     def get_num_items_in_cart(self):
         num = 0
@@ -88,13 +91,14 @@ class ShoppingCart:
         print("{}'s Shopping Cart - {}".format(self.customer_name, self.current_date))
         num = 0
         for item in self.cart_items:
-            num += item.quantity
+            num += int(item.quantity)
         print("Number of Items: {}\n".format(num))
         total = 0
         if len(self.cart_items) == 0:
             print("SHOPPING CART IS EMPTY")
         for item in self.cart_items:
-            total += item.print_item_cost()
+            item.print_item_cost()
+            total += item.total_cost()
         print("\nTotal: ${}".format(total))
 
 
@@ -133,4 +137,3 @@ if __name__ == "__main__":
     customer_cart = ShoppingCart(customer_name, todays_date)
 
     print_menu(customer_cart)
-
